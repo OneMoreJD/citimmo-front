22101085f0159f6c7c655012e2509e336efcf139
duvaudierje <jerome.duvaudier@natixis.com>
2021-09-22 14:55:59 +0200

    creation of quick search form

--- src/app/home/quick-search/quick-search.component.ts
+++ src/app/home/quick-search/quick-search.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-quick-search',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuickSearchComponent implements OnInit {
 
-  constructor() { }
+  quickSearchForm: FormGroup;
+  transactionControl = new FormControl('buy');
+  locationControl = new FormControl('', Validators.required);
+  budgetControl = new FormControl('', Validators.pattern('^[0-9]*$'));
+
+  url: string = 'http://localhost:3000';
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.quickSearchForm = this.formBuilder.group({
+      transaction: this.transactionControl,
+      location: this.locationControl,
+      budget: this.budgetControl
+    });
   }
 
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('transaction', this.transactionControl.value);
+    formData.append('location', this.locationControl.value);
+    formData.append('budget', this.budgetControl.value);
+
+    this.http.post(this.url, formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+  }
+
+  getLocationErrorMessage() {
+    if (this.locationControl.hasError('required')) {
+      return 'Ce champ est obligatoire';
+    }
+  }
 }
