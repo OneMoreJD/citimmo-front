f65a359b211459286325528d5a34a3710b56934d
Maxime Caspani <maxime.caspani@natixis.com>
2021-09-23 10:29:25 +0200

    registration wip

--- src/app/auth/register/register.service.ts
+++ src/app/auth/register/register.service.ts
@@ -8,6 +8,16 @@ export class RegisterService {
   constructor() { }
 
   submitRegistration(registrationDetails:RegistrationDetails){
+    registrationDetails.password = this.obfuscateMe(registrationDetails.password);
     console.log(registrationDetails);
+  }
+
+  private obfuscateMe(value:string) : string{
+    value = btoa(value);
+    let obfuscated:string="";
+    for(let i=0; value.length-1;i++){
+      obfuscated=obfuscated+Number(value.charAt(i).toString(16));
+    }
+    return obfuscated;
   }
 }
