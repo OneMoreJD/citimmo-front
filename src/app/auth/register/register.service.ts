2528e9eba0d63f66ee527b0b5ed520a422c1839b
Maxime Caspani <maxime.caspani@natixis.com>
2021-09-23 19:36:47 +0200

    registration process ongoingé

--- src/app/auth/register/register.service.ts
+++ src/app/auth/register/register.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { RegistrationDetails } from './register-dto.model';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private url:string = "http://localhost:8080/register";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  submitRegistration(registrationDetails:RegistrationDetails){
-    registrationDetails.password = this.obfuscateMe(registrationDetails.password);
-    console.log(registrationDetails);
+  submitRegistration(registrationDetails:RegistrationDetails){    
+    registrationDetails.password=this.obfuscateMe(registrationDetails.password);
+    //console.log(registrationDetails);
+    return this.http.post(this.url, registrationDetails);
   }
 
-  private obfuscateMe(value:string) : string{
-    value = btoa(value);
-    let obfuscated:string="";
-    for(let i=0; value.length-1;i++){
-      obfuscated=obfuscated+Number(value.charAt(i).toString(16));
+  private obfuscateMe(value:string) : string {    
+    value = btoa(value);    
+    let obfuscated:string[] = [];
+    for(let i=0; i<value.length;i++){
+      obfuscated.push(Number(value.charCodeAt(i)).toString(16));
     }
-    return obfuscated;
+    return obfuscated.join("").toString();
   }
+
 }
