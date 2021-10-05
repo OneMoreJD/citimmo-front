b7cc6ca1f0fe7bd09233285e0177bf2fe0f16b2e
duvaudierje <jerome.duvaudier@natixis.com>
2021-10-05 17:17:59 +0200

    creation of fake data

--- src/app/adverts/adverts-list/adverts-list.component.ts
+++ src/app/adverts/adverts-list/adverts-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { QuickSearchComponent } from '../../home/quick-search/quick-search.component';
+import { environment } from 'src/environments/environment';
 import { QuickSearchService } from '../../home/quick-search/quick-search.service';
 
 @Component({
@@ -12,42 +11,40 @@ export class AdvertsListComponent implements OnInit {
 
   adverts: any[] = [];
 
-  constructor(private router: Router, private qsService: QuickSearchService) {
-    // if (this.router.getCurrentNavigation().extras.state) {
-    //   const routeState = this.router.getCurrentNavigation().extras.state;
-    //   if (routeState) {
-    //     this.adverts = routeState.adverts ? JSON.parse(routeState.adverts) : [];
-    //     console.log(this.adverts);
-    //   }
-    // }
+  constructor(private qsService: QuickSearchService) {
+
+    this.adverts = this.qsService.adverts;
+    
+    if (!environment.production) {      
+      if (!this.adverts) {
+        this.adverts = [];
+        this.adverts.push({
+          title: 'tres bel appartement',
+          bedrooms: 3,
+          rooms: 5,
+          description: 'Très bel appartement\nA visiter absolument',
+          address: {
+            city: 'Paris'
+          },
+          price: 850000,
+          surfaceInt: 167
+        },
+        {
+          title: '3 pièces lumineux',
+          bedrooms: 1,
+          rooms: 3,
+          description: 'Très bel appartement\nA visiter absolument',
+          address: {
+            city: 'Paris'
+          },
+          price: 650000,
+          surfaceInt: 74
+        });
+      }
+    }
   }
 
   ngOnInit(): void {
-    this.adverts = this.qsService.adverts;
-    if (this.adverts.length === 0) {
-      this.adverts.push({
-        title: 'tres bel appartement',
-        bedrooms: 3,
-        rooms: 5,
-        description: 'Très bel appartement\nA visiter absolument',
-        address: {
-          city: 'Paris'
-        },
-        price: 850000,
-        surfaceInt: 167
-      },
-      {
-        title: '3 pièces lumineux',
-        bedrooms: 1,
-        rooms: 3,
-        description: 'Très bel appartement\nA visiter absolument',
-        address: {
-          city: 'Paris'
-        },
-        price: 650000,
-        surfaceInt: 74
-      });
-    }
   }
 
 }
