61c44d00e2005671c9fe99fd88026703cad09a42
duvaudierje <jerome.duvaudier@natixis.com>
2021-10-05 17:18:22 +0200

    creation of estate type form control

--- src/app/adverts/search/search.component.ts
+++ src/app/adverts/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { QuickSearchService } from '../../home/quick-search/quick-search.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -9,12 +10,28 @@ import { QuickSearchService } from '../../home/quick-search/quick-search.service
 export class SearchComponent implements OnInit {
 
   currentCriteria: any;
+  estateTypeList: string[] = [
+    'Maison',
+    'Appartement',
+    'Garage',
+    'Péniche'
+  ];
 
-  constructor(private qsService: QuickSearchService) { }
+  searchForm: FormGroup;
+  estateTypeControl = new FormControl();
+
+  constructor(private qsService: QuickSearchService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.currentCriteria = this.qsService.criteria;
     console.log(this.currentCriteria);
+
+    this.searchForm = this.formBuilder.group({
+      estateType: this.estateTypeControl
+    });
   }
 
+  test(event:any) {
+    console.log(event);
+  }
 }
